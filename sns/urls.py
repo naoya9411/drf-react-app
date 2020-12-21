cf05efaaa1ebfd226ae151b15fefb2378753bf7b
naoya <sn7sn8@gmail.com>
2020-12-21 21:03:04 +0900

    kousinn

--- sns/urls.py
+++ sns/urls.py
@@ -19,7 +19,7 @@ from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+## 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authen/', views.obtain_auth_token),
